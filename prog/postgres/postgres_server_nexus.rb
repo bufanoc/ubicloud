48b7474edc297e17386754a5ca584487f0657b83
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2026-03-26 04:52:03 +0100

    Inline Postgres::Restart prog
    We hit lots of bugs due to bud/push mechanism.
    
    When push is used, it adds a new frame to the stack and all the relevant
    code needs to take that into account. For example, simply checking the
    state of the strand becomes tricky.
    
    When bud is used, it creates child strands and the code needs to take
    that into account as well. It needs to ensure that child strands are
    reaped properly. However we might have budded different children at
    different points in the code and it becomes hard to track them.
    
    Restart is guilty of both. We both bud and push it in different places.
    It is no surprise that we had number of live site issues because of it.
    Recently, we moved it to a separate prog, which helped but didn't solve
    the underlying problem. We still had to take into account the fact that
    it might have been pushed or budded.
    
    As a more extreme solution, I'm refactoring restart to be a method. It
    saves us from all the complexity explained above. As a trade off, now
    callers need to handle that it needs to be called multiple times. The
    first call triggers the restart and second (or subsequent) calls check
    if it is completed. However, I think this is a much more manageable
    complexity than the previous one. We had 3 places where we call restart
    and in one of them, tracking the state would require lots of changes.
    Thankfully, it was possible to eliminate that call altogether (did in
    the previous commits).
    
    I also added a new deadline for restart to complete. However, since it
    doesn't have its own label anymore, it is not possible to go to that
    label and setting the deadline to arrive back to wait. Instead, I added
    a dummy label name in the deadline and used the new unregister_deadline
    helper to clear it when restart is completed.

--- prog/postgres/postgres_server_nexus.rb
+++ prog/postgres/postgres_server_nexus.rb
@@ -569,8 +569,13 @@ SQL
     end
 
     when_restart_set? do
-      decr_restart
-      push Prog::Postgres::Restart
+      register_deadline("complete_restart", 2 * 60)
+      if daemonized_restart
+        decr_restart
+        unregister_deadline("complete_restart")
+      else
+        nap 1
+      end
     end
 
     when_refresh_walg_credentials_set? do
@@ -624,9 +629,6 @@ SQL
       hop_configure
     end
 
-    reap(fallthrough: true)
-    nap 5 unless strand.children_dataset.where(prog: "Postgres::Restart").empty?
-
     if available?
       decr_checkup
       decr_recycle_unavailable_server
@@ -635,7 +637,7 @@ SQL
 
     postgres_server.incr_recycle_unavailable_server unless postgres_server.recycle_unavailable_server_set?
 
-    bud Prog::Postgres::Restart
+    daemonized_restart
     nap 5
   end
 
@@ -829,5 +831,17 @@ SQL
 
   def version
     postgres_server.version
+  end
+
+  def daemonized_restart
+    case vm.sshable.d_check("postgres_restart")
+    when "Succeeded"
+      vm.sshable.d_clean("postgres_restart")
+      return true
+    when "Failed", "NotStarted"
+      vm.sshable.d_run("postgres_restart", "sudo", "postgres/bin/restart", postgres_server.version)
+    end
+
+    false
   end
 end
