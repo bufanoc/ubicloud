8de0912e0f1f94a7da35912ca26289e1588c85ad
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2026-03-26 04:38:58 +0100

    Don't start the Postgres server in cluster creation
    We used to need this to be able to run queries to set up replication and
    pgbouncer. However, post-installation-script handles that instead. Also
    keeping --start here prevents configure to restart the cluster, instead
    it just does reload. So, we are removing --start flag.

--- rhizome/postgres/lib/postgres_setup.rb
+++ rhizome/postgres/lib/postgres_setup.rb
@@ -34,6 +34,6 @@ class PostgresSetup
   end
 
   def create_cluster
-    r "pg_createcluster #{@version} main --port=5432 --start --locale=C.UTF8"
+    r "pg_createcluster #{@version} main --port=5432 --locale=C.UTF8"
   end
 end
